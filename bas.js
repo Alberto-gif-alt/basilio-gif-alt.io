d9c0338b75a917db06f8a7a9265528eb1abd575c
albetoooo <albert_les_@hotmail.com>
2024-11-17 21:48:20 -0400

    Add files via upload

---
+++ bas.js
@@ -0,0 +1,15 @@
+function enviar() {
+	var der=document.getElementById('der1').value;
+	var der2=document.getElementById('der3').value;
+	if (der=="alberto") {
+		if (der2=="basilio") {
+		alert("exsacto");
+	location.href="lista.html"
+		
+	}
+	else
+		alert("error");
+}
+	
+	
+}
---
+++ enviar.js
@@ -0,0 +1,14 @@
+function enviar() {
+	var n1=document.getElementByid('der').value;
+	var n2=document.getElementByid('der1').value;
+	if (n1=='alberto') {
+		if (n2=='basilio') {
+		alert('exsacto');
+		
+	}
+	else
+		alert('error');
+}
+	
+	
+}
